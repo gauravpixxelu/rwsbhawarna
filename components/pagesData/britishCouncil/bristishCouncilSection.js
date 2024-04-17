a49b60ff3196154130450cbd40d9899320affcbb
karanbhardwaj0028 <karanbhardwaj0028@gmail.com>
2024-04-17 16:58:05 +0530

    new code

---
+++ components/pagesData/britishCouncil/bristishCouncilSection.js
@@ -0,0 +1,13 @@
+import BritishStyle from './bristish.module.css'
+
+const BritishCouncilSection = () => {
+    return (
+        <section className={BritishStyle.british_sec}>
+            <div className="container">
+
+            </div>
+        </section>
+    );
+};
+
+export default BritishCouncilSection;
---
+++ pages/activities/british-council.js
@@ -0,0 +1,16 @@
+import Footer from '@/components/footer/footer';
+import Header from '@/components/header/header';
+import BannerSection from '@/components/pagesData/britishCouncil/bannerSection';
+
+function BritishCouncil() {
+    return (
+        <>
+            <Header />
+            <BannerSection />
+            
+            <Footer />
+        </>
+    )
+}
+
+export default BritishCouncil
